167b827bbcc276ec19136651b176389252684b4a
Josh Drummond <joshdrummond@users.noreply.github.com>
2022-02-05 18:32:35 -0800

    Update Export-Hakchi2CEGameList.ps1
    Added CSV file output, better sorting

--- Export-Hakchi2CEGameList.ps1
+++ Export-Hakchi2CEGameList.ps1
@@ -1,4 +1,4 @@
-﻿<#
+<#
 .Synopsis
 	Generates Hakchi2 CE Game List.
 	
@@ -8,7 +8,7 @@
 	
 .Notes
 	Author        : Josh Drummond
-	Version       : 1.0 - 12/22/2021 - Initial release
+	Version       : 1.1 - 2/5/2022 - Add CSV output
 	Source        : https://github.com/joshdrummond/Hakchi2CEGameListExporter/
 	
 .Example
@@ -33,39 +33,40 @@ if (($ConsoleType -ge 0) -and ($ConsoleType -le 9))
 }
 
 # Parse Files to Game List
-$GameList = @{}
+$GameList = @()
 Foreach ($Game in $HakchiConfig.gamesCollectionSettings.$ConsoleTypeName.SelectedGames)
 {
     if (Test-Path -Path .\games\$Game\$Game.desktop)
     {
-        $GameInfo = @{}
+        $GameInfo = New-Object PSObject
         $data = Get-Content -Raw -Path .\games\$Game\metadata.json | ConvertFrom-Json
-        $GameInfo.add('Filename', $data.OriginalFilename)
+        $GameInfo | Add-Member -MemberType NoteProperty -Name "Filename" -Value $data.OriginalFilename
         if ($data.System)
         {
-            $GameInfo.add('System', $data.System)
+            $GameInfo | Add-Member -MemberType NoteProperty -Name "System" -Value $data.System
         }
         else
         {
-            $GameInfo.add('System', $data.Core)
+            $GameInfo | Add-Member -MemberType NoteProperty -Name "System" -Value $data.Core
         }
         $data = Get-Content -Path .\games\$Game\$Game.desktop
-        $GameInfo.add('Name', $data[4].Substring($data[4].IndexOf("=")+1))
-        $GameList.add($Game, $GameInfo)
+        $GameInfo | Add-Member -MemberType NoteProperty -Name "Name" -Value $data[4].Substring($data[4].IndexOf("=")+1)
+        $GameList += $GameInfo
     }
 }
 Foreach ($Game in $HakchiConfig.gamesCollectionSettings.$ConsoleTypeName.OriginalGames)
 {
     if (Test-Path -Path .\games_originals\$Game\$Game.desktop)
     {
-        $GameInfo = @{}
+        $GameInfo = New-Object PSObject
         $data = Get-Content -Path .\games_originals\$Game\$Game.desktop
-        $GameInfo.add('Name', $data[4].Substring($data[4].IndexOf("=")+1))
-        $GameInfo.add('Filename', 'Original')
-        $GameInfo.add('System', $ConsoleTypeName)
-        $GameList.add($Game, $GameInfo)
+        $GameInfo | Add-Member -MemberType NoteProperty -Name "Name" -Value $data[4].Substring($data[4].IndexOf("=")+1)
+        $GameInfo | Add-Member -MemberType NoteProperty -Name "Filename" -Value "Original"
+        $GameInfo | Add-Member -MemberType NoteProperty -Name "System" -Value $ConsoleTypeName
+        $GameList += $GameInfo
     }
 }
+$GameList = $GameList | Sort-Object -Property Name
 
 #Generate HTML File
 $HTML = @"
@@ -91,7 +92,7 @@ pre{width:80%;margin:30px auto;}
 function TableSort(t){if(this.tbl=document.getElementById(t),this.lastSortedTh=null,this.tbl&&"TABLE"==this.tbl.nodeName){for(var e=this.tbl.tHead.rows[0].cells,a=0;e[a];a++)e[a].className.match(/asc|dsc/)&&(this.lastSortedTh=e[a]);this.makeSortable()}}
 function bubbleSort(t,e){var a,s;1===e?(a=0,s=t.length):-1===e&&(a=t.length-1,s=-1);for(var r=!0;r;){r=!1;for(var l=a;l!=s;l+=e)if(t[l+e]&&t[l].value>t[l+e].value){var o=t[l],c=t[l+e],n=o;t[l]=c,t[l+e]=n,r=!0}}return t}
 TableSort.prototype.makeSortable=function(){for(var t=this.tbl.tHead.rows[0].cells,e=0;t[e];e++){t[e].cIdx=e;var a=document.createElement("a");a.href="#",a.title="Sort",a.style.display="block",a.innerHTML=t[e].innerHTML,a.onclick=function(t){return function(){return t.sortCol(this),!1}}(this),t[e].innerHTML="",t[e].appendChild(a)}},TableSort.prototype.sortCol=function(t){for(var e=this.tbl.rows,a=[],s=[],r=0,l=0,o=t.parentNode,c=o.cIdx,n=1;e[n];n++){var i=e[n].cells[c],h=i.textContent?i.textContent:i.innerText,b=h.replace(/(\$|\,|\s)/g,"");parseFloat(b)==b?s[l++]={value:Number(b),row:e[n]}:a[r++]={value:h,row:e[n]}}var d,u,m;o.className.match("asc")?(u=bubbleSort(a,-1),m=bubbleSort(s,-1),o.className=o.className.replace(/asc/,"dsc")):(u=bubbleSort(s,1),m=bubbleSort(a,1),o.className.match("dsc")?o.className=o.className.replace(/dsc/,"asc"):o.className+="asc"),this.lastSortedTh&&o!=this.lastSortedTh&&(this.lastSortedTh.className=this.lastSortedTh.className.replace(/dsc|asc/g,"")),this.lastSortedTh=o,d=u.concat(m);var S=this.tbl.tBodies[0];for(n=0;d[n];n++)S.appendChild(d[n].row)},
-window.onload=function(){new TableSort("gamelist");var rows=document.getElementById("gamelist").rows.length;document.getElementById("total").innerHTML=rows;};
+window.onload=function(){new TableSort("gamelist");var rows=document.getElementById("gamelist").rows.length-1;document.getElementById("total").innerHTML=rows;};
 </script>
 </head>
 <body>
@@ -100,12 +101,14 @@ window.onload=function(){new TableSort("gamelist");var rows=document.getElementB
 <thead>
 <tr><th>Title&nbsp;<span>&#x25BC;&#x25B2;</span></th><th>System&nbsp;<span>&#x25BC;&#x25B2;</span></th><th>Filename&nbsp;<span>&#x25BC;&#x25B2;</span></th></tr>
 </thead>
-<tbody>
+<tbody>`n
 "@
-Foreach ($Game in ($GameList.Keys | Sort))
+Foreach ($Game in $GameList)
 {
-    $HTML += "<tr><td>"+$GameList.$Game.Name+"</td><td>"+$GameList.$Game.System+"</td><td>"+$GameList.$Game.Filename+"</td></tr>`n"
+    $HTML += "<tr><td>"+$Game.Name+"</td><td>"+$Game.System+"</td><td>"+$Game.Filename+"</td></tr>`n"
 }
 $HTML += "</tbody></table><b>Total number of games: <span id='total'></span></b></body></html>"
-
 $HTML | Out-File -Encoding utf8 '.\ClassicMiniGameList.html'
+
+#Generate CSV File
+$GameList | select Name,System,Filename | Export-Csv -Path '.\ClassicMiniGameList.csv' -NoTypeInformation
